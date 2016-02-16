3e548f9e6f39486bd3caa6fa643f011942f767dd
Nabeela Jaffer <njaffer@umich.edu>
2016-02-15 22:00:13 -0500

    config changes

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Umrdr
     end
 
     # Deploy to /data instead of /
-    config.relative_url_root = '/data'
+#    config.relative_url_root = '/data'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
